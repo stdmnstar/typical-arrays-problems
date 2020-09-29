1d0b2de818d92947e72085bcf04a70103bd36bb5
Aliaksandr Tarhonski <stdmnstar@gmail.com>
2020-09-29 17:12:16 +0300

    feat: solution task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
+exports.min = function min(array) {
+    if ((!array) || (array.length == 0)) {
+        return 0;
+    }
+    let min = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] < min) {
+            min = array[i];
+        }
+    }
+    return min;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if ((!array) || (array.length == 0)) {
+        return 0;
+    }
+    let max = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] > max) {
+            max = array[i];
+        }
+    }
+    return max;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if ((!array) || (array.length == 0)) {
+        return 0;
+    }
+    return array.reduce((summa, arrayItem) => summa += arrayItem, 0) / array.length;
+};
